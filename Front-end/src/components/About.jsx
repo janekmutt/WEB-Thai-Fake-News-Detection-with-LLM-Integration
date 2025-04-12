c268d6b9f966fd994897d935a768ba3fae68ce99
SolitudeX39 <cmanwood.m@gmail.com>
2025-04-12 21:04:51 +0700

    Revert "Old Updates with Working Basic Backend Integration"
    This reverts commit e6f52c015df2bdc87ee64998b6f9b21a2537240c.

--- Front-end/src/components/About.jsx
+++ Front-end/src/components/About.jsx
@@ -1,34 +1,3 @@
-import React from "react";
-import Navbar from "./Navbar";
-import "../App.css";
-
-function About() {
-  return (
-    <>
-      <Navbar />
-      <main className="about-container">
-        <section
-          className="about-section"
-          role="region"
-          aria-label="About Us Section"
-        >
-          <img
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/5587dc42420e498758513fb4530651f0d144a6925f279bcd8ce089b07796ab10?placeholderIfAbsent=true&apiKey=29c095f8129d4027a45c332c76e3f7d7"
-            alt="About Us"
-            className="about-image"
-          />
-          <div className="about-header">
-            <h1 className="about-header-text">เกี่ยวกับเรา</h1>
-            <div className="about-box" />
-          </div>
-        </section>
-      </main>
-    </>
-  );
-}
-
-export default About;
 // import React from "react";
 // import Navbar from "./Navbar";
 // import "../App.css";
@@ -62,3 +31,35 @@ export default About;
 // }
 
 // export default About;
+
+import React from "react";
+import Navbar from "./Navbar";
+import "../App.css";
+
+function About() {
+  return (
+    <>
+      <Navbar />
+      <main className="about-container">
+        <section
+          className="about-section"
+          role="region"
+          aria-label="About Us Section"
+        >
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/5587dc42420e498758513fb4530651f0d144a6925f279bcd8ce089b07796ab10?placeholderIfAbsent=true&apiKey=29c095f8129d4027a45c332c76e3f7d7"
+            alt="About Us"
+            className="about-image"
+          />
+          <div className="about-header">
+            <h1 className="about-header-text">เกี่ยวกับเรา</h1>
+            <div className="about-box" />
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default About;
